2f2f80b4218dc1ec524e0dd73334ab70c30a8993
kimnux12 <kimnux@gmail.com>
2024-07-25 19:37:54 +0900

    2024-07-25, 19:34

--- .config/doom/init.el
+++ .config/doom/init.el
@@ -86,7 +86,7 @@
        vterm             ; the best terminal emulation in Emacs
 
        :checkers
-       (syntax +childframe)  ; tasing you for every semicolon you forget(this include Flycheck)
+       (syntax +everywhere +childframe)  ; tasing you for every semicolon you forget(this include Flycheck)
        ;;(spell +flyspell) ; tasing you for misspelling mispelling
        ;;grammar           ; tasing grammar mistake every you make
 
